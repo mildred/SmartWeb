e1177598a33f769e46d29ddaa0be1dc471d37bc6
Shanti Bouchez-Mongardé <shanti-pub.git@mildred.fr>
2015-04-19 11:36:25 +0200

    WIP

--- server/server.go
+++ server/server.go
@@ -2,12 +2,15 @@ package server
 
 import (
 	"net/http"
+	"filepath"
 )
 
 type SmartServer struct {
 	DataPath string
 }
 
-func (server SmartServer) ServeHTTP(http.ResponseWriter, *http.Request) {
+func (server SmartServer) ServeHTTP(http.ResponseWriter, req *http.Request) {
+	var p := filepath.Join(server.DataPath, filepath.Clean(req.URL.Path))
 
+	
 }
